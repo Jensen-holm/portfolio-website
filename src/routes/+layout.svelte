e92b26a0613998e1347290f34696687cf0229180
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-04 21:35:16 -0400

    transfering repo from org to personal

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.postcss';
+	import Footer from '../components/footer.svelte';
+	import Navbar from '../components/navbar.svelte';
+</script>
+
+<Navbar />
+<div class="pt-20">
+	<slot />
+</div>
+<Footer />
