3fc2a57cfcf788d7dba7ad14a17715f12ac1d1d7
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-05 11:10:33 -0400

    made the card grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,41 @@
 <script>
-  import { Card, Button } from "flowbite-svelte";
+  import { Card, GradientButton, Heading } from "flowbite-svelte";
   import { ArrowRightOutline } from "flowbite-svelte-icons";
 </script>
 
 <div>
-  <div class="pt-20 pb-10">
-    <h1 class="text-4xl font-bold text-center">Projects</h1>
+  <div class="pt-10 pb-5">
+    <div class="text-center">
+      <Heading
+        tag="h2"
+        class="mb-4"
+        customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl"
+        >Projects</Heading
+      >
+    </div>
   </div>
 
-  <div class="h-screen flex justify-center items-center">
+  <div
+    class="h-screen grid grid-cols-2 gap-x-0 gap-y-0 justify-items-center items-center mt-10 mb-10 mx-auto w-6/12"
+  >
+    <div class="">
+      <Card class="pt-4">
+        <h5
+          class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+        >
+          Reversing Audio Files
+        </h5>
+        <p
+          class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight"
+        >
+          Reversing audio files and listening them in reverse can be fun. Try it
+          and see if you find any hidden messages in any .wav files you have ...
+        </p>
+        <GradientButton class="w-fit" href="/audio_reverse">
+          Try it<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
+        </GradientButton>
+      </Card>
+    </div>
     <div class="mx-5">
       <Card class="pt-4">
         <h5
@@ -22,9 +49,9 @@
           Reversing audio files and listening them in reverse can be fun. Try it
           and see if you find any hidden messages in any .wav files you have ...
         </p>
-        <Button class="w-fit" href="/audio_reverse">
+        <GradientButton class="w-fit" href="/audio_reverse">
           Try it<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
-        </Button>
+        </GradientButton>
       </Card>
     </div>
     <div class="mx-5">
@@ -41,9 +68,9 @@
           some hyper parameter tuning with this neural network on the iris
           dataset.
         </p>
-        <Button class="w-fit" href="/audio_reverse">
+        <GradientButton class="w-fit" href="/audio_reverse">
           Try it<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
-        </Button>
+        </GradientButton>
       </Card>
     </div>
     <div class="mx-5">
@@ -59,9 +86,12 @@
           I am working on a wrapper for the G3N graphics engine library with a
           built in physics engine. Written in Go
         </p>
-        <Button class="w-fit" href="https://github.com/Jensen-holm/g3n-wrapper">
+        <GradientButton
+          class="w-fit"
+          href="https://github.com/Jensen-holm/g3n-wrapper"
+        >
           Source Code<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
-        </Button>
+        </GradientButton>
       </Card>
     </div>
   </div>
