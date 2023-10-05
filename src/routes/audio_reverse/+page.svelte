8a6aabc754e2b5668f3b569eeb9f4bcf857d3711
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-05 14:40:00 -0400

    now working on sending post request and adding reverse button

--- src/routes/audio_reverse/+page.svelte
+++ src/routes/audio_reverse/+page.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
-  import { GradientButton } from "flowbite-svelte";
+  import { Card } from "flowbite-svelte";
   import { sound } from "svelte-sound";
-  import nfl from "../../audio/nfl-on-espn.wav";
-  import jeff from "../../audio/my-name-is-jeff.wav";
+  import jeff from "../../static/audio/my-name-is-jeff.wav";
+  import axios from "axios";
+
+  let audioSource = jeff;
 </script>
 
-<button use:sound={{ src: nfl, events: ["click"] }}> Click Me!! </button>
-<button use:sound={{ src: jeff, events: ["click"] }}> Click Me!! </button>
+<div class="flex justify-center items-center pt-20">
+  <Card>
+    <img src="mnij.png" alt="my name is jeff button picture" />
+
+    <button
+      use:sound={{ src: audioSource, events: ["click"] }}
+      type="button"
+      class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    >
+      Play
+    </button>
+  </Card>
+</div>
