1f3bc5d9677613a718251b67d85d988b0fe0ee2f
Jensen-holm <holmj@mail.gvsu.edu>
2023-10-06 15:10:43 -0400

    implementing a project component

---
+++ src/components/project.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import { Card, GradientButton } from "flowbite-svelte";
+  import { ArrowRightOutline } from "flowbite-svelte-icons";
+
+  export let projectName: string;
+  export let projectImgPath: string;
+  export let buttonText: string;
+  export let description: string;
+  export let href: string;
+  export let buttonHref: string;
+</script>
+
+<Card {href} class="mb-4">
+  <img
+    src={projectImgPath}
+    alt="Neural Network from scratch"
+    class="w-full h-auto object-contain"
+  />
+  <h5
+    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+  >
+    {projectName}
+  </h5>
+  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+    {description}
+  </p>
+  <GradientButton class="w-fit" href={buttonHref}>
+    {buttonText}<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
+  </GradientButton>
+</Card>
