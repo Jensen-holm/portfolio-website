796f7e297596ba3f96b1f8a8872c096d7af61320
Jensen-holm <holmj@mail.gvsu.edu>
2023-11-21 11:56:42 -0500

    adding experience images with links and tooltips

--- src/components/project_page/project.svelte
+++ src/components/project_page/project.svelte
@@ -1,37 +1,42 @@
 <script lang="ts">
-  import { Card, GradientButton, Badge } from "flowbite-svelte";
-  import { ArrowRightOutline } from "flowbite-svelte-icons";
+    import {Card, GradientButton, Badge} from "flowbite-svelte";
+    import {ArrowRightOutline} from "flowbite-svelte-icons";
 
-  export let projectName: string;
-  export let projectImgPath: string;
-  export let buttonText: string = "Try it";
-  export let description: string = "";
-  export let href: string;
-  export let buttonHref: string;
-  export let inProgress: boolean = false;
-  export let newTab: boolean = false;
+    export let projectName: string;
+    export let projectImgPath: string;
+    export let buttonText: string = "Try it";
+    export let description: string = "";
+    export let href: string;
+    export let buttonHref: string;
+    export let inProgress: boolean = false;
+    export let newTab: boolean = false;
 </script>
 
-<Card {href} target={newTab ? "_blank" : ""} class="mb-4">
-  <img
-    src={projectImgPath}
-    alt="Neural Network from scratch"
-    class="w-full h-auto object-contain"
-  />
-  <h5
-    class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-  >
-    {projectName}
-  </h5>
-  {#if inProgress}
-    <Badge color="red">In Progress</Badge>
-  {:else}
-    <Badge color="green">Complete</Badge>
-  {/if}
-  <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
-    {description}
-  </p>
-  <GradientButton class="w-fit" href={buttonHref} target="_blank">
-    {buttonText}<ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white" />
-  </GradientButton>
+<Card {href}
+      target={newTab ? "_blank" : ""}
+      class="mb-4">
+    <img
+            src={projectImgPath}
+            alt="Neural Network from scratch"
+            class="w-full h-auto object-contain"
+    />
+    <h5
+            class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+        {projectName}
+    </h5>
+    {#if inProgress}
+        <Badge color="red">In Progress</Badge>
+    {:else}
+        <Badge color="green">Complete</Badge>
+    {/if}
+    <p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">
+        {description}
+    </p>
+    <GradientButton class="w-fit"
+                    href={buttonHref}
+                    target="_blank">
+        {buttonText}
+        <ArrowRightOutline class="w-3.5 h-3.5 ml-2 text-white"/>
+    </GradientButton>
 </Card>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,74 +1,90 @@
 <script>
-  import Project from "../components/project_page/project.svelte";
-
-  import { Heading, Img, GradientButton } from "flowbite-svelte";
-  import Readme from "../components/general/readme.svelte";
-
-  let resume = "jensen-holm-resume.pdf";
+    import {Heading, Img, Tooltip} from "flowbite-svelte";
+    import Project from "../components/project_page/project.svelte";
+    import Readme from "../components/general/readme.svelte";
 </script>
 
 <div class="flex justify-center items-center">
-  <div class="pt-20">
-    <Img src="school.png" />
-  </div>
+    <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 max-lg:grid-cols-3">
+        <div class="pt-10">
+            <Img src="white-pine-digital.jpeg"
+                 class="w-40 h-40 object-cover"/>
+            <Tooltip>Experience: Full Stack Software Engineer Intern @ <a href="https://whitepine.digital/">White Pine
+                Digital</a></Tooltip>
+        </div>
+        <div class="pt-10">
+            <Img src="gvsu_roster_pic.png"
+                 class="w-40 h-40 object-cover"/>
+            <Tooltip>Me</Tooltip>
+        </div>
+        <div class="pt-10">
+            <Img src="kalamazoo-growlers.png"
+                 class="w-40 h-40 object-cover"/>
+            <Tooltip>Experience: Data Science / Analytics Intern @
+                <a href="https://northwoodsleague.com/kalamazoo-growlers/">Kalamazoo Growlers</a></Tooltip>
+        </div>
+    </div>
 </div>
 
 <div class="flex justify-center items-center">
-  <Readme
-    readmeUrl="https://raw.githubusercontent.com/Jensen-holm/Jensen-holm/main/README.md"
-  />
+    <Readme
+            readmeUrl="https://raw.githubusercontent.com/Jensen-holm/Jensen-holm/main/README.md"
+    />
 </div>
 
 <div class="min-h-screen h-screen">
-  <div class="pt-10 pb-5">
-    <div class="text-center">
-      <Heading tag="h2" class="mb-4" customSize="text-6xl font-extrabold p-10"
-        >Projects</Heading
-      >
+    <div class="pt-10 pb-5">
+        <div class="text-center">
+            <Heading tag="h2"
+                     class="mb-4"
+                     customSize="text-6xl font-extrabold p-10"
+            >Projects
+            </Heading
+            >
+        </div>
     </div>
-  </div>
 
-  <div class="flex justify-center items-center">
-    <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-4">
-      <Project
-        projectName="Neural network algorithm from scratch"
-        projectImgPath="tree-backprop.gif"
-        buttonText="Try it"
-        buttonHref="/machine_learning_from_scratch"
-        href="/machine_learning_from_scratch"
-        description="Test out a neural network classification algorithm I built from scratch using your own data."
-      />
+    <div class="flex justify-center items-center">
+        <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-4">
+            <Project
+                    projectName="Neural network algorithm from scratch"
+                    projectImgPath="tree-backprop.gif"
+                    buttonText="Try it"
+                    buttonHref="/machine_learning_from_scratch"
+                    href="/machine_learning_from_scratch"
+                    description="Test out a neural network classification algorithm I built from scratch using your own data."
+            />
 
-      <Project
-        projectName="G3N Graphics library wrapper & physics engine"
-        projectImgPath="g3n-wrapper.gif"
-        buttonText="Source code"
-        buttonHref="https://github.com/Jensen-holm/g3n-wrapper"
-        href="/g3n-wrapper"
-        description="Wrapper package with built in physics engine for the G3N graphics package in Go."
-        inProgress={false}
-        newTab={true}
-      />
+            <Project
+                    projectName="G3N Graphics library wrapper & physics engine"
+                    projectImgPath="g3n-wrapper.gif"
+                    buttonText="Source code"
+                    buttonHref="https://github.com/Jensen-holm/g3n-wrapper"
+                    href="/g3n-wrapper"
+                    description="Wrapper package with built in physics engine for the G3N graphics package in Go."
+                    inProgress={false}
+                    newTab={true}
+            />
 
-      <Project
-        projectName="Monte Carlo baseball time machine"
-        projectImgPath="bball_time_machine.gif"
-        buttonText="Try it"
-        buttonHref="https://jensen-holm-mlb-app-app-q1fw6t.streamlit.app/"
-        href="https://jensen-holm-mlb-app-app-q1fw6t.streamlit.app/"
-        description="Baseball Monte Carlo simulation web-app. Simulate baseball teams from different era's against each other for up to 1,620 games"
-        newTab={true}
-      />
+            <Project
+                    projectName="Monte Carlo baseball time machine"
+                    projectImgPath="bball_time_machine.gif"
+                    buttonText="Try it"
+                    buttonHref="https://jensen-holm-mlb-app-app-q1fw6t.streamlit.app/"
+                    href="https://jensen-holm-mlb-app-app-q1fw6t.streamlit.app/"
+                    description="Baseball Monte Carlo simulation web-app. Simulate baseball teams from different era's against each other for up to 1,620 games"
+                    newTab={true}
+            />
 
-      <Project
-        projectName="My Portfolio Website"
-        projectImgPath="svelte-kit-tailwind.jpg"
-        buttonText="Source code"
-        buttonHref="/webpage"
-        href="/webpage"
-        description="I built this website using svelte-kit and tailwind css for a simple, lightweight, & responsive application."
-        inProgress={true}
-      />
+            <Project
+                    projectName="My Portfolio Website"
+                    projectImgPath="svelte-kit-tailwind.jpg"
+                    buttonText="Source code"
+                    buttonHref="/webpage"
+                    href="/webpage"
+                    description="I built this website using svelte-kit and tailwind css for a simple, lightweight, & responsive application."
+                    inProgress={true}
+            />
+        </div>
     </div>
-  </div>
 </div>
