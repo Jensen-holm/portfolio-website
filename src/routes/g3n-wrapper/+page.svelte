692ba994858474f0400b80274b71e134cdaed1cf
Jensen-holm <holmj@mail.gvsu.edu>
2024-01-30 12:56:44 -0500

    init commit for python htmx setup.

--- src/routes/g3n-wrapper/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-  import Readme from "../../components/general/readme.svelte";
-</script>
-
-<div class="flex justify-center items-center p-10">
-  <Readme
-    readmeUrl={"https://raw.githubusercontent.com/Jensen-holm/g3n-Wrapper/main/README.md"}
-  ></Readme>
-</div>
